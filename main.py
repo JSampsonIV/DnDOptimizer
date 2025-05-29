41c002ce21df2b5f99b351a10fdaa4c2b96be45a
JSampsonIV <johnlsampson4@gmail.com>
2025-05-29 12:00:37 -0400

    removed testing lines in main

--- main.py
+++ main.py
@@ -4,6 +4,3 @@ from CharacterClass import CharacterClass
 from Species import Species
 from Character import Character
 from SpellList import SpellList
-
-arrako = Character('Arrako', 'Cleric', 'Tiefling', 5, 14, 12, 17, 15, 20, 16, ['Cure Wounds', 'Guiding Bolt', 'Shield of Faith'])
-print(f"Known Spells: {arrako.get_known_spells()}")
